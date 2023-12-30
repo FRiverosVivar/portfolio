994a91357fd7bbfa3ec2a2e6fb4d9832e4e1880d
Franco Riveros <francoriverosvivar@gmail.com>
2023-12-30 14:34:04 -0300

    updated footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,8 +9,7 @@ const Footer = () => {
       <Socials></Socials>
       <Article classNames="flex text-center max-w-2xl text-gray-300 font-poppins">
         Loosely designed in Figma and coded in Visual Studio Code by yours
-        truly. Built with Next.js and Tailwind CSS, deployed with Vercel. All
-        text is set in the Inter typeface.
+        truly. Built with Next.js and Tailwind CSS, deployed with Vercel.
       </Article>
     </footer>
   );
